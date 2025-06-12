c0c4b713e705fe0e4a52cafe73a89d91ccea3d31
Eduardo Garcia <edugarciasb16@gmail.com>
2025-06-12 04:03:03 +0200

    feat(ui): add PhoneCard component to display product info

---
+++ src/features/phones/components/PhoneCard.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+import { Product } from '../types';
+
+export function PhoneCard({ phone }: { phone: Product }) {
+  return (
+    <div className="border-[0.5px] h-[340px] border-text-primary p-4 flex flex-col justify-between">
+      <div className="w-full h-[290px] flex items-center justify-center mb-4">
+        <Image
+          src={phone.imageUrl}
+          alt={phone.name}
+          width={200}
+          height={200}
+          style={{
+            width: 'auto',
+            height: 'auto',
+            maxHeight: '200px',
+            maxWidth: '200px',
+          }}
+        />
+      </div>
+      <div className="text-xs uppercase font-light text-text-muted mb-1">
+        {phone.brand}
+      </div>
+      <div className="flex justify-between">
+        <div className="text-sm font-light uppercase text-primary">
+          {phone.name}
+        </div>
+        <div className="text-sm font-light text-primary">
+          {phone.basePrice} EUR
+        </div>
+      </div>
+    </div>
+  );
+}
