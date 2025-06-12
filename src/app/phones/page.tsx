104c8f7e1aa7fa0e57a4dc5345ea49b86828b680
Eduardo Garcia <edugarciasb16@gmail.com>
2025-06-12 04:04:11 +0200

    feat: add implement grid layout

--- src/app/phones/page.tsx
+++ src/app/phones/page.tsx
@@ -1,9 +1,21 @@
+'use client';
+import { useState } from 'react';
 import { SearchBar } from '@/features/phones/components/SearchBar';
+import { PhoneGrid } from '@/features/phones/components/PhoneGrid';
+import { usePhones } from '@/features/phones/hooks/usePhone';
+import { ResultCount } from '@/features/phones/components/ResultCount';
+import { useDebounce } from '@/hooks/useDebounce';
 
 export default function PhonesPage() {
+  const [search, setSearch] = useState('');
+  const debouncedSearch = useDebounce(search, 400);
+  const { phones, isLoading } = usePhones(debouncedSearch);
+
   return (
-    <>
-      <SearchBar />
-    </>
+    <div>
+      <SearchBar search={search} setSearch={setSearch} />
+      <ResultCount count={phones.length} />
+      {isLoading ? <p>Cargando...</p> : <PhoneGrid phones={phones} />}
+    </div>
   );
 }
