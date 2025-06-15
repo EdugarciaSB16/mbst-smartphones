bbf57e7ba76b2376d53abb9879b8970842742963
Eduardo Garcia <edugarciasb16@gmail.com>
2025-06-15 21:32:04 +0200

    feat: add jest configuration

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|scss|sass)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+};
+
+export default config;
